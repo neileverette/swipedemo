8050dab2d798567e6944a30643e440488f683bd0
neileverette <neil.everette@gmail.com>
2014-06-19 13:21:03 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
 $(document).ready(function(){
-
-	$.event.special.swipe.durationThreshold=500;
-	$.event.special.swipe.horizontalDistanceThreshold=50;
-
-	 
-	$(document).on('swipeleft', '[data-role="page"]', function(event){    
-	    if(event.handled !== true) // This will prevent event triggering more then once
-	    {    
-	        var nextpage = $(this).next('[data-role="page"]');
-	        // swipe using id of next page if exists
-	        if (nextpage.length > 0) {
-	            $.mobile.changePage(nextpage, {transition: "slide", reverse: false}, true, true);
-	        }
-	        event.handled = true;
-	    }
-	    return false;         
-	});
-	
-	$(document).on('swiperight', '[data-role="page"]', function(event){   
-	    if(event.handled !== true) // This will prevent event triggering more then once
-	    {      
-	        var prevpage = $(this).prev('[data-role="page"]');
-	        if (prevpage.length > 0) {
-	            $.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
-	        }
-	        event.handled = true;
-	    }
-	    return false;            
-	});
+		
+	$(".labels").draggable({ axis: "y"});
 
 });
